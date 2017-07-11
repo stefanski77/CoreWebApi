6b7caa85d84a877918f075359908e0dba6b66818
Stefan Bell <stefan.bell@gmail.com>
2017-07-11 22:14:51 +0200

    initalize connection string

--- CoreWebApi/PostgresAdapter.cs
+++ CoreWebApi/PostgresAdapter.cs
@@ -1,12 +1,18 @@
-﻿using System;
-using Npgsql;
+﻿using Npgsql;
 
 namespace CoreWebApi
 {
     public class PostgresAdapter
     {
+        private readonly NpgsqlConnectionStringBuilder connectionStringBuilder;
+
         public PostgresAdapter()
         {
+            connectionStringBuilder = new NpgsqlConnectionStringBuilder()
+            {
+                Host = "PostgreSQL 9.6",
+                Database = "Stefan"
+            };
         }
     }
 }
